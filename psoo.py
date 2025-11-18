3fc68d914cde5cb5c49c0f22eff5e26547acfb14
Antonio-CMA <acmayala@gmail.com>
2025-11-18 15:35:43 -0600

    Update psoo.py

--- psoo.py
+++ psoo.py
@@ -1,9 +1,9 @@
 """This file is called psoo, for particle swarm optimization optimization, as it optimizes the c1 and c2 values (dumb placeholder but psoo is whimsical idc)"""
 
 import pso, random
-dimension = 2
-print("Version 2.4.1")
-
+print("Version 2.4.2")
+x0 = [5, 5]
+bounds = [(-10,10), (-10,10)]
 
 def initialize():
 	#This will hold all of our data in tuples that stores the c1, c2, and finish time values
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 			num_particles = 20
 			pso_c1 = random.choices(c1_values, weights = c1_weights, k = 1)[0]
 			pso_c2 = random.choices(c2_values, weights = c2_weights, k = 1)[0]
-	        data_pool.append(pso.run(num_particles, pso_c1, pso_c2, best_finish_time, worst_finish_time, dimension))
+	        data_pool.append(pso.run(num_particles, pso_c1, pso_c2, best_finish_time, worst_finish_time, x0, bounds))
 	
 	    #This assumes the time will be the first item in the tuple, but can be changed from 0-2
 	    sorted_data_pool = sorted(data_pool, key = lambda item: item[0])
